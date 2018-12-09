233401916deefdc44760617459d6ce3c9e851669
SHY <thgusdud123@ajou.ac.kr>
2018-12-09 20:56:42 +0900

    minor report

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 require('./db/mongo') //connect db
-// const doctor = require('./routes/doctor')
-// const patient = require('./routes/patient')
 
 const user = require('./routes/user')
 const search = require('./routes/search')
 const mainpage = require('./routes/mainpage')
+
 const app = express()
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*")
@@ -17,9 +16,9 @@ app.use((req, res, next) =>{
 })
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-// app.use('/doctor', doctor)
-// app.use('/patient', patient)
+
 app.use('/search', search)
 app.use('/user', user)
 app.use('/mainpage',mainpage)
+
 module.exports = app
